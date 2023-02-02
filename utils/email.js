4e4867b67eafd57d043791818c1097dd04a72ce0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-02 02:14:22 +0100

    fix: format

--- utils/email.js
+++ utils/email.js
@@ -3,62 +3,62 @@ const htmlToText = require('html-to-text');
 const ejs = require('ejs');
 
 module.exports = class Email {
-    constructor(user, url) {
-        this.to = user.email;
-        this.firstName = user.name.split(' ')[0];
-        this.url = url;
-        this.from = `John Doe ${process.env.EMAIL_FROM}`;
-    }
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `John Doe ${process.env.EMAIL_FROM}`;
+  }
 
-    newTransport() {
-        if (process.env.NODE_ENV === 'production') {
-            // SENDGRID
-            return nodemailer.createTransport({
-                service: 'SendGrid',
-                auth: {
-                    user: process.env.SENDGRID_USERNAME,
-                    pass: process.env.SENDGRID_PASSWORD
-                }
-            });
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      // sendgrid
+      return nodemailer.createTransport({
+        service: 'SendGrid',
+        auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD
         }
-        // MAILTRAP
-        return nodemailer.createTransport({
-            host: process.env.EMAIL_HOST,
-            port: process.env.EMAIL_PORT,
-            auth: {
-                user: process.env.EMAIL_USERNAME,
-                pass: process.env.EMAIL_PASSWORD
-            }
-        });
+      });
     }
+    // mailtrap
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD
+      }
+    });
+  }
 
-    // SEND THE ACTUAL EMAIL
-    async send(template, subject) {
-        // RENDER HTML BASED ON EJS TEMPLATE
-        const html = ejs.renderFile(`${__dirname}/../views/email/${template}.ejs`, {
-            firstName: this.firstName,
-            url: this.url,
-            subject
-        });
+  // send the actual email
+  async send(template, subject) {
+    // render html based on ejs template
+    const html = ejs.renderFile(`${__dirname}/../views/email/${template}.ejs`, {
+      firstName: this.firstName,
+      url: this.url,
+      subject
+    });
 
-        // DEFINE EMAIL OPTIONS
-        const mailOptions = {
-            from: this.from,
-            to: this.to,
-            subject,
-            html,
-            text: htmlToText.fromString(html)
-        };
+    // define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.fromString(html)
+    };
 
-        // CREATE A TRANSPORT AND SEND EMAIL
-        await this.newTransport().sendMail(mailOptions);
-    }
+    // create a transpaort and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
 
-    async sendWelcome() {
-        await this.send('welcome', 'Welcome to ecommerce shopping cart');
-    }
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to ecommerce shopping cart');
+  }
 
-    async sendPasswordReset() {
-        await this.send('passwordReset', 'Your password reset token (valid for only 10 minutes)');
-    }
+  async sendPasswordReset() {
+    await this.send('passwordReset', 'Your password reset token (valid for only 10 minutes)');
+  }
 }
