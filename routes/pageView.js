85c6e2b53fe9416380c2205575b4440774360607
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-02 02:05:21 +0100

    fix: format

--- routes/pageView.js
+++ routes/pageView.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const authController = require('../controllers/authController');
 const pageViewController = require('../controllers/pageViewController');
 const Page = require('../models/Page');
