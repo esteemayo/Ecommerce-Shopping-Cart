2714634dc4176c6a1e79892e4bbd59d6a7272e3e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-02 02:05:44 +0100

    fix: format

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,14 @@
 const express = require('express');
+
 const authController = require('../controllers/authController');
 const userController = require('../controllers/userController');
 
 const router = express.Router();
 
 router.post('/signup',
-    authController.uploadUserPhoto,
-    authController.resizeUserPhoto,
-    authController.signup
+  authController.uploadUserPhoto,
+  authController.resizeUserPhoto,
+  authController.signup
 );
 
 router.post('/login', authController.login);
@@ -25,9 +26,9 @@ router.patch('/updateMyPassword', authController.updatePassword);
 router.get('/me', userController.getMe, userController.getUser);
 
 router.patch('/updateMe',
-    userController.uploadUserPhoto,
-    userController.resizeUserPhoto,
-    userController.updateMe
+  userController.uploadUserPhoto,
+  userController.resizeUserPhoto,
+  userController.updateMe
 );
 
 router.delete('/deleteMe', userController.deleteMe);
@@ -35,14 +36,14 @@ router.delete('/deleteMe', userController.deleteMe);
 router.use(authController.restrictTo('admin'));
 
 router
-    .route('/')
-    .get(userController.getAllUser)
-    .post(userController.createUser)
+  .route('/')
+  .get(userController.getAllUser)
+  .post(userController.createUser)
 
 router
-    .route('/:id')
-    .get(userController.getUser)
-    .patch(userController.updateUser)
-    .delete(userController.deleteUser)
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser)
 
 module.exports = router;
