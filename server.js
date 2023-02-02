862618acdf805e7edc76a854ea61d8eea199a23b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-02 02:08:28 +0100

    fix: format

--- server.js
+++ server.js
@@ -2,9 +2,9 @@
 // const dotenv = require('dotenv');
 
 process.on('uncaughtException', err => {
-    console.log('UNCAUGHT EXCEPTION! Shutting down...');
-    console.log(err.name, err.message);
-    process.exit(1);
+  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
 });
 
 // dotenv.config({ path: './config.env' });
@@ -17,9 +17,9 @@ const PORT = process.env.PORT || 9000;
 const server = app.listen(PORT, () => console.log(`APP LISTENING ON PORT ${PORT}`));
 
 process.on('unhandledRejection', err => {
-    console.log('UNHANDLED REJECTION! Shutting down...');
-    console.log(err.name, err.message);
-    server.close(() => {
-        process.exit(1);
-    });
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
