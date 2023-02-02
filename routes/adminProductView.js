5e7b027eb70076fd7220fa7d0815b182008a08c9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-02 01:37:40 +0100

    fix: modify

--- routes/adminProductView.js
+++ routes/adminProductView.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const multer = require('multer');
+
 const authController = require('../controllers/authController');
 const adminProductViewController = require('../controllers/adminProductViewController');
 
