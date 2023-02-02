1cfa3f0712090247450dd19abd3e5fad3a675adb
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-02 02:31:59 +0100

    fix: format

--- controllers/pageViewController.js
+++ controllers/pageViewController.js
@@ -3,24 +3,24 @@ const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
 exports.indexPage = catchAsync(async (req, res, next) => {
-    const page = await Page.findOne({ slug: 'home' });
+  const page = await Page.findOne({ slug: 'home' });
 
-    res.status(200).render('index', {
-        title: page.title,
-        content: page.content
-    });
+  res.status(200).render('index', {
+    title: page.title,
+    content: page.content
+  });
 });
 
 // GET OTHER PAGES (ABOUT, SERVICES, BLOG, CONTACT US etc...)
 exports.page = catchAsync(async (req, res, next) => {
-    const page = await Page.findOne({ slug: req.params.slug });
+  const page = await Page.findOne({ slug: req.params.slug });
 
-    // if (!page) {
-    //     return next(new AppError('No page found with that slug', 404));
-    // }
+  // if (!page) {
+  //     return next(new AppError('No page found with that slug', 404));
+  // }
 
-    res.status(200).render('index', {
-        title: page.title,
-        content: page.content
-    });
+  res.status(200).render('index', {
+    title: page.title,
+    content: page.content
+  });
 });
