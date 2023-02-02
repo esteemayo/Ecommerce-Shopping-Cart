66c10f5f4fad010c072c844736ce7ab60317225a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-02 01:36:07 +0100

    fix: modify

--- models/Product.js
+++ models/Product.js
@@ -57,6 +57,7 @@ productSchema.pre('save', function (next) {
   next();
 });
 
-const Product = mongoose.model('Product', productSchema);
+const Product = mongoose.models.Product ||
+  mongoose.model('Product', productSchema);
 
 module.exports = Product;
