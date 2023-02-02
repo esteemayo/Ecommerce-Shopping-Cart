81ae742ee46a1e93b2ae42597276b0d6efd4484f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-02 01:47:23 +0100

    fix: modify

--- routes/adminCategoryView.js
+++ routes/adminCategoryView.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const authController = require('../controllers/authController');
 const adminCategoryViewController = require('../controllers/adminCategoryViewController');
 
