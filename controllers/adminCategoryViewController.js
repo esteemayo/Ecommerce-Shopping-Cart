2976e017d925967557727c75f7afe9856ebfd5ae
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-02 02:31:24 +0100

    fix: format

--- controllers/adminCategoryViewController.js
+++ controllers/adminCategoryViewController.js
@@ -3,29 +3,29 @@ const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
 exports.getAllCategories = catchAsync(async (req, res, next) => {
-    const categories = await Category.find();
+  const categories = await Category.find();
 
-    res.status(200).render('admin/categories', {
-        title: '',
-        categories
-    });
+  res.status(200).render('admin/categories', {
+    title: '',
+    categories
+  });
 });
 
 exports.addCategory = (req, res) => {
-    res.status(200).render('admin/add_category', {
-        title: 'Add new category'
-    });
+  res.status(200).render('admin/add_category', {
+    title: 'Add new category'
+  });
 };
 
 exports.getEditCategory = catchAsync(async (req, res, next) => {
-    const category = await Category.findById(req.params.id);
+  const category = await Category.findById(req.params.id);
 
-    if (!category) {
-        return next(new AppError('No category found with that ID', 404));
-    }
+  if (!category) {
+    return next(new AppError('No category found with that ID', 404));
+  }
 
-    res.status(200).render('admin/edit_category', {
-        title: '',
-        category
-    });
+  res.status(200).render('admin/edit_category', {
+    title: '',
+    category
+  });
 });
