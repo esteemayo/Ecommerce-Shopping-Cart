2105696d2a84f6afa2ae90f15af7691c4e096565
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-02 02:08:36 +0100

    fix: format

--- startup/routes.js
+++ startup/routes.js
@@ -31,107 +31,107 @@ const adminCategoryView = require('../routes/adminCategoryView');
 const adminPageView = require('../routes/adminPageView');
 
 module.exports = app => {
-    app.set('view engine', 'ejs');
-    app.set('views', path.join(__dirname, '../views'));
+  app.set('view engine', 'ejs');
+  app.set('views', path.join(__dirname, '../views'));
 
-    // GLOBAL MIDDLEWARES
+  // GLOBAL MIDDLEWARES
 
-    // SERVING STATIC FILES
-    app.use(express.static(path.join(__dirname, '../public')));
+  // SERVING STATIC FILES
+  app.use(express.static(path.join(__dirname, '../public')));
 
-    // SET SECURITY HTTP HEADERS
-    app.use(helmet());
+  // SET SECURITY HTTP HEADERS
+  app.use(helmet());
 
-    // DEVELOPMENT LOGGING
-    if (process.env.NODE_ENV === 'development') {
-        app.use(morgan('dev'));
-    }
+  // DEVELOPMENT LOGGING
+  if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+  }
 
-    // EXPRESS SESSION MIDDLEWARE
-    app.use(session({
-        secret: 'secret',
-        resave: false,
-        saveUninitialized: false
-    }));
+  // EXPRESS SESSION MIDDLEWARE
+  app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+  }));
 
-    // LIMIT REQUEST FROM SOME API
-    const limiter = rateLimit({
-        max: 100,
-        windowMs: 60 * 60 * 1000, // 1 Hour
-        message: 'Too many requests from this IP, Please try again in an hour!'
-    });
-    app.use('/api', limiter);
+  // LIMIT REQUEST FROM SOME API
+  const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000, // 1 Hour
+    message: 'Too many requests from this IP, Please try again in an hour!'
+  });
+  app.use('/api', limiter);
 
-    // app.use((req, res, next) => {
-    //     res.locals.user = req.user;
-    //     next();
-    // });
+  // app.use((req, res, next) => {
+  //     res.locals.user = req.user;
+  //     next();
+  // });
 
-    // GET ALL PAGES TO HEADER.EJS
-    app.use(catchAsync(async (req, res, next) => {
-        const pages = await Page.find().sort({ sorting: 1 });
-        res.locals.pages = pages;
-        res.locals.user = authController.isLoggedIn;
-        next();
-    }));
+  // GET ALL PAGES TO HEADER.EJS
+  app.use(catchAsync(async (req, res, next) => {
+    const pages = await Page.find().sort({ sorting: 1 });
+    res.locals.pages = pages;
+    res.locals.user = authController.isLoggedIn;
+    next();
+  }));
 
-    app.use((req, res, next) => {
-        res.locals.cart = req.session.cart;
-        res.locals.user = authController.isLoggedIn;
-        res.locals.url = req.originalUrl;
-        next();
-    });
+  app.use((req, res, next) => {
+    res.locals.cart = req.session.cart;
+    res.locals.user = authController.isLoggedIn;
+    res.locals.url = req.originalUrl;
+    next();
+  });
 
-    // GET ALL CATEGORIES TO HEADER.EJS
-    app.use(async (req, res, next) => {
-        const categories = await Category.find();
-        res.locals.categories = categories;
-        next();
-    });
+  // GET ALL CATEGORIES TO HEADER.EJS
+  app.use(async (req, res, next) => {
+    const categories = await Category.find();
+    res.locals.categories = categories;
+    next();
+  });
 
-    // BODY PARSER
-    app.use(express.json({ limit: '10kb' }));
-    app.use(express.urlencoded({ extended: true, limit: '10kb' }));
-    app.use(cookieParser());
+  // BODY PARSER
+  app.use(express.json({ limit: '10kb' }));
+  app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+  app.use(cookieParser());
 
-    // DATA SANITIZATION AGAINST NoSQL QUERY INJECTION
-    app.use(mongoSanitize());
+  // DATA SANITIZATION AGAINST NoSQL QUERY INJECTION
+  app.use(mongoSanitize());
 
-    // DATA SANITIZATION AGAINST XSS
-    app.use(xss());
+  // DATA SANITIZATION AGAINST XSS
+  app.use(xss());
 
-    // PREVENT PARAMETER POLLUTION
-    app.use(hpp({
-        whitelist: [
-            'price',
-            'priceDiscount',
-            'title'
-        ]
-    }));
+  // PREVENT PARAMETER POLLUTION
+  app.use(hpp({
+    whitelist: [
+      'price',
+      'priceDiscount',
+      'title'
+    ]
+  }));
 
-    // TEST MIDDLEWARE
-    app.use((req, res, next) => {
-        req.requestTime = new Date().toISOString();
-        // console.log(req.headers);
-        // console.log(req.cookies);
-        next();
-    });
+  // TEST MIDDLEWARE
+  app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    // console.log(req.headers);
+    // console.log(req.cookies);
+    next();
+  });
 
-    app.use('/admin/pages', adminPageView);
-    app.use('/admin/categories', adminCategoryView);
-    app.use('/admin/products', adminProductView);
-    app.use('/products', productView);
-    app.use('/cart', cartRoute);
-    app.use('/', viewRoute);
-    app.use('/', pageView);
-    app.use('/api/v1/categories', categoryRoute);
-    app.use('/api/v1/pages', pageRoute);
-    app.use('/api/v1/products', productRoute);
-    app.use('/api/v1/users', usersRoute);
+  app.use('/admin/pages', adminPageView);
+  app.use('/admin/categories', adminCategoryView);
+  app.use('/admin/products', adminProductView);
+  app.use('/products', productView);
+  app.use('/cart', cartRoute);
+  app.use('/', viewRoute);
+  app.use('/', pageView);
+  app.use('/api/v1/categories', categoryRoute);
+  app.use('/api/v1/pages', pageRoute);
+  app.use('/api/v1/products', productRoute);
+  app.use('/api/v1/users', usersRoute);
 
-    app.all('*', (req, res, next) => {
-        next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
-    });
+  app.all('*', (req, res, next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+  });
 
-    app.use(globalErrorHandler);
+  app.use(globalErrorHandler);
 }
