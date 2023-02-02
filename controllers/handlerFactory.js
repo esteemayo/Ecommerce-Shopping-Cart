96103e32ea628bfbbfb01d144a58231dcbb0e349
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-02 02:31:40 +0100

    fix: format

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -3,95 +3,95 @@ const APIFeatures = require('../utils/apiFeatures');
 const AppError = require('../utils/appError');
 
 exports.getAll = Model => catchAsync(async (req, res, next) => {
-    // console.log(req.query);
-    const features = new APIFeatures(Model.find(), req.query)
-        .filter()
-        .sort()
-        .limitFields()
-        .paginate();
+  // console.log(req.query);
+  const features = new APIFeatures(Model.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
 
-    const doc = await features.query;
-    // const doc = await features.query.explain();
+  const doc = await features.query;
+  // const doc = await features.query.explain();
 
-    res.status(200).json({
-        status: 'success',
-        requestedAt: req.requestTime,
-        results: doc.length,
-        data: {
-            doc
-        }
-    });
+  res.status(200).json({
+    status: 'success',
+    requestedAt: req.requestTime,
+    results: doc.length,
+    data: {
+      doc
+    }
+  });
 });
 
 exports.getOne = Model => catchAsync(async (req, res, next) => {
-    const doc = await Model.findById(req.params.id);
+  const doc = await Model.findById(req.params.id);
 
-    if (!doc) {
-        return next(new AppError('No document found with that ID', 404));
+  if (!doc) {
+    return next(new AppError('No document found with that ID', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      doc
     }
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-            doc
-        }
-    });
+  });
 });
 
 exports.createOne = Model => catchAsync(async (req, res, next) => {
-    let doc = req.body;
+  let doc = req.body;
 
-    if (req.file) {
-        doc.image = req.file.filename;
-    } else if (req.files) {
-        doc.galleryImage = req.files.filename;
+  if (req.file) {
+    doc.image = req.file.filename;
+  } else if (req.files) {
+    doc.galleryImage = req.files.filename;
+  }
+
+  doc = await Model.create(doc);
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      doc
     }
-
-    doc = await Model.create(doc);
-
-    res.status(201).json({
-        status: 'success',
-        data: {
-            doc
-        }
-    });
+  });
 });
 
 exports.updateOne = Model => catchAsync(async (req, res, next) => {
-    let doc = req.body;
+  let doc = req.body;
 
-    if (req.file) {
-        doc.image = req.file.filename;
-    } else if (req.files) {
-        doc.galleryImage = req.files.filename;
+  if (req.file) {
+    doc.image = req.file.filename;
+  } else if (req.files) {
+    doc.galleryImage = req.files.filename;
+  }
+
+  doc = await Model.findByIdAndUpdate(req.params.id, doc, {
+    new: true,
+    runValidators: true
+  });
+
+  if (!doc) {
+    return next(new AppError('No document found with the ID', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      doc
     }
-
-    doc = await Model.findByIdAndUpdate(req.params.id, doc, {
-        new: true,
-        runValidators: true
-    });
-
-    if (!doc) {
-        return next(new AppError('No document found with the ID', 404));
-    }
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-            doc
-        }
-    });
+  });
 });
 
 exports.deleteOne = Model => catchAsync(async (req, res, next) => {
-    const doc = await Model.findByIdAndDelete(req.params.id);
+  const doc = await Model.findByIdAndDelete(req.params.id);
 
-    if (!doc) {
-        return next(new AppError('No document found with the ID', 404));
-    }
+  if (!doc) {
+    return next(new AppError('No document found with the ID', 404));
+  }
 
-    res.status(204).json({
-        status: 'success',
-        data: null
-    });
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
 });
