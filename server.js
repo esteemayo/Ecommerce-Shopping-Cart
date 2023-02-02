2cb0491534cd098c4c1ea187b3b2b1874f01a4f3
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-02 02:09:26 +0100

    fix: modify

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
-// const mongoose = require('mongoose');
-// const dotenv = require('dotenv');
+const app = require('./app');
+require('./startup/db')();
 
 process.on('uncaughtException', err => {
   console.log('UNCAUGHT EXCEPTION! Shutting down...');
   console.log(err.name, err.message);
   process.exit(1);
 });
-
-// dotenv.config({ path: './config.env' });
-const app = require('./app');
-
-require('./startup/db')();
 
 const PORT = process.env.PORT || 9000;
 
