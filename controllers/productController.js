5db722f7a7dbea5b7b3db9acdecdcc83a79fb1f0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-02 02:32:13 +0100

    fix: format

--- controllers/productController.js
+++ controllers/productController.js
@@ -21,52 +21,52 @@ const multerStorage = multer.diskStorage({
 const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, cb) => {
-    if (file.mimetype.startsWith('image')) {
-        return cb(null, true);
-    }
-    cb(new AppError('Not an image! Please upload only images', 400), false);
+  if (file.mimetype.startsWith('image')) {
+    return cb(null, true);
+  }
+  cb(new AppError('Not an image! Please upload only images', 400), false);
 };
 
 const upload = multer({
-    storage: multerStorage,
-    fileFilter: multerFilter
+  storage: multerStorage,
+  fileFilter: multerFilter
 });
 
 exports.uploadProductImage = upload.fields([
-    { name: 'image', maxCount: 1 },
-    { name: 'imageGallery', maxCount: 4 }
+  { name: 'image', maxCount: 1 },
+  { name: 'imageGallery', maxCount: 4 }
 ]);
 
 exports.resizeProductImages = catchAsync(async (req, res, next) => {
-    // console.log(req.files);
+  // console.log(req.files);
 
-    if (!req.files.image || !req.files.imageGallery) return next();
+  if (!req.files.image || !req.files.imageGallery) return next();
 
-    // PRODUCT IMAGE
-    req.body.image = `product-${req.params.id}-${Date.now()}.jpeg`;
-    await sharp(req.files.image[0].buffer)
+  // PRODUCT IMAGE
+  req.body.image = `product-${req.params.id}-${Date.now()}.jpeg`;
+  await sharp(req.files.image[0].buffer)
+    .resize(2000, 1333)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/products/${req.body.image}`);
+
+  // PRODUCT IMAGEGALLERY
+  req.body.imageGallery = [];
+  await Promise.all(
+    req.files.imageGallery.map(async (file, i) => {
+      const filename = `gallery-${req.params.id}-${Date.now()}-${i + 1}.jpeg`;
+
+      await sharp(file.buffer)
         .resize(2000, 1333)
         .toFormat('jpeg')
         .jpeg({ quality: 90 })
-        .toFile(`public/img/products/${req.body.image}`);
+        .toFile(`public/img/gallery/${filename}`);
 
-    // PRODUCT IMAGEGALLERY
-    req.body.imageGallery = [];
-    await Promise.all(
-        req.files.imageGallery.map(async (file, i) => {
-            const filename = `gallery-${req.params.id}-${Date.now()}-${i + 1}.jpeg`;
+      req.body.imageGallery.push(filename);
+    })
+  );
 
-            await sharp(file.buffer)
-                .resize(2000, 1333)
-                .toFormat('jpeg')
-                .jpeg({ quality: 90 })
-                .toFile(`public/img/gallery/${filename}`);
-
-            req.body.imageGallery.push(filename);
-        })
-    );
-
-    next();
+  next();
 });
 
 exports.getAllProducts = factory.getAll(Product);
@@ -76,33 +76,33 @@ exports.updateProduct = factory.updateOne(Product);
 exports.deleteProduct = factory.deleteOne(Product);
 
 exports.getProductBySlug = catchAsync(async (req, res, next) => {
-    const products = await Product.find({ category: req.params.category });
+  const products = await Product.find({ category: req.params.category });
 
-    if (!products) {
-        return next(new AppError('No product found with the given SLUG', 404));
+  if (!products) {
+    return next(new AppError('No product found with the given SLUG', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    results: products.length,
+    data: {
+      products
     }
-
-    res.status(200).json({
-        status: 'success',
-        results: products.length,
-        data: {
-            products
-        }
-    });
+  });
 });
 
 // Nested route
 exports.getProductByCategory = catchAsync(async (req, res, next) => {
-    const products = await Product.find({ category: req.params.category, slug: req.params.product });
+  const products = await Product.find({ category: req.params.category, slug: req.params.product });
 
-    if (!products) {
-        return next(new AppError('No product found in that category', 404));
+  if (!products) {
+    return next(new AppError('No product found in that category', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      products
     }
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-            products
-        }
-    });
+  });
 });
