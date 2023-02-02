0100ae272292067210422cd6bc898264c5244138
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-02 01:45:57 +0100

    fix: format

--- models/Category.js
+++ models/Category.js
@@ -1,27 +1,27 @@
-const mongoose = require('mongoose');
 const slugify = require('slugify');
+const mongoose = require('mongoose');
 
 const categorySchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: [true, 'A category must have a title'],
-        unique: true,
-        trim: true,
-        maxlength: [20, 'A category title must have less or equal than 20 characters'],
-        minlength: [4, 'A category title must have more or equal than 5 characters']
-    },
-    slug: String
+  title: {
+    type: String,
+    required: [true, 'A category must have a title'],
+    unique: true,
+    trim: true,
+    maxlength: [20, 'A category title must have less or equal than 20 characters'],
+    minlength: [4, 'A category title must have more or equal than 5 characters'],
+  },
+  slug: String
 }, {
-    toJSON: { virtuals: true },
-    toObject: { virtuals: true }
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true }
 });
 
-// DOCUMENT MIDDLEWARE
 categorySchema.pre('save', function (next) {
-    this.slug = slugify(this.title, { lower: true });
-    next();
+  this.slug = slugify(this.title, { lower: true });
+  next();
 });
 
-const Category = mongoose.model('Category', categorySchema);
+const Category = mongoose.models.Category ||
+  mongoose.model('Category', categorySchema);
 
 module.exports = Category;
