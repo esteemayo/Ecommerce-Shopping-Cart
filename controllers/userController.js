b0fbeaacc3a92add0cf8f9abc30e99b3380284c9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-02 01:39:53 +0100

    fix: format

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,83 +21,81 @@ const multerStorage = multer.diskStorage({
 const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, cb) => {
-    if (file.mimetype.startsWith('image')) {
-        return cb(null, true);
-    }
-    cb(new AppError('Not an image! Please upload only images', 400), false);
+  if (file.mimetype.startsWith('image')) {
+    return cb(null, true);
+  }
+  cb(new AppError('Not an image! Please upload only images', 400), false);
 };
 
 const upload = multer({
-    storage: multerStorage,
-    fileFilter: multerFilter
+  storage: multerStorage,
+  fileFilter: multerFilter
 });
 
 exports.uploadUserPhoto = upload.single('photo');
 
 exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
-    if (!req.file) return next();
+  if (!req.file) return next();
 
-    req.file.fiename = `user-${req.user.id}-${Date.now()}.jpeg`;
+  req.file.fiename = `user-${req.user.id}-${Date.now()}.jpeg`;
 
-    await sharp(req.file.buffer)
-        .resize(500, 500)
-        .toFormat('jpeg')
-        .jpeg({ quality: 90 })
-        .toFile(`public/img/users/${req.file.fiename}`);
+  await sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/users/${req.file.fiename}`);
 
-    next();
+  next();
 });
 
 exports.updateMe = catchAsync(async (req, res, next) => {
-    // console.log(req.file);
-    // console.log(req.body);
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use update /updateMyPassword',
+        400,
+      )
+    );
+  }
 
-    // CREATE ERROR IF USER POSTS PASSWORD DATA
-    if (req.body.password || req.body.passwordConfirm) {
-        return next(new AppError('This route is not for password updates. Please use update /updateMyPassword', 400));
+  let filterBody = _.pick(req.body, 'name', 'email', 'username');
+  if (req.file) filterBody.photo = req.file.fiename;
+
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filterBody, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser
     }
-
-    // FILTERED OUT UNWANTED FILEDs NAMES THAT ARE NOT ALLOWED TO BE UPDATED
-    let filterBody = _.pick(req.body, 'name', 'email', 'username');
-    if (req.file) filterBody.photo = req.file.fiename;
-    // UPDATE USER DOCUMENT
-    const updatedUser = await User.findByIdAndUpdate(req.user.id, filterBody, {
-        new: true,
-        runValidators: true
-    });
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-            user: updatedUser
-        }
-    });
+  });
 });
 
-// GET THE CURRENTLY LOGGED IN USER
 exports.getMe = (req, res, next) => {
-    req.params.id = req.user.id;
-    next();
+  req.params.id = req.user.id;
+  next();
 };
 
 exports.deleteMe = catchAsync(async (req, res, next) => {
-    await User.findByIdAndUpdate(req.user.id, { active: false });
+  await User.findByIdAndUpdate(req.user.id, { active: false });
 
-    res.status(204).json({
-        status: 'success',
-        data: null
-    });
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
 });
 
 exports.createUser = (req, res) => {
-    res.status(500).json({
-        status: 'fail',
-        message: `This route is not defined! Please use ${req.protocol}://${req.get('host')}/api/v1/users/signup instead`
-    });
+  res.status(500).json({
+    status: 'fail',
+    message: `This route is not defined! Please use ${req.protocol}://${req.get('host')}/api/v1/users/signup instead`
+  });
 };
 
 exports.getAllUser = factory.getAll(User);
 exports.getUser = factory.getOne(User);
-// DO NOT UPDATE PASSWORD WITH THIS
 exports.updateUser = factory.updateOne(User);
 exports.deleteUser = factory.deleteOne(User);
