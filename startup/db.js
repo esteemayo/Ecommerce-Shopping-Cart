5eeee872c116f489d6869c45c6feaafd25a1ac9f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-06 15:54:29 +0100

    fix: refactor

--- startup/db.js
+++ startup/db.js
@@ -3,18 +3,26 @@ const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
 
-const db = process.env.DATABASE_LOCAL;
+const devEnv = process.env.NODE_ENV !== 'production';
+const { DATABASE, DATABASE_LOCAL, DATABASE_PASSWORD } = process.env;
 
-module.exports = () => {
-  mongoose.connect(db, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-  })
-    .then(cons => {
-      // console.log(cons.connections);
-      console.log('MongoDB Connected...');
-    })
-    .catch(err => console.log(`COULD NOT CONNECT TO MONGODB: ${err}`));
+const mongoURI = DATABASE.replace('<PASSWORD>', DATABASE_PASSWORD);
+const dbLocal = DATABASE_LOCAL;
+
+const db = devEnv ? dbLocal : mongoURI;
+
+const connectDb = async () => {
+  try {
+    const conn = await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    });
+    console.log(`MongoDB Connected → ${conn.connection.port}`);
+  } catch (err) {
+    throw err;
+  }
 };
+
+module.exports = connectDb;
