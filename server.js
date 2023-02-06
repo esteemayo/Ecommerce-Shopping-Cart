844105b3fcabbe90505abce38dbd05d767b80cae
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-06 15:29:00 +0100

    fix: covert back to common JS

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
-import app from './app.js';
-// require('./startup/db')();
+const app = require('./app');
+require('./startup/db')();
 
 process.on('uncaughtException', err => {
-  console.log('UNCAUGHT EXCEPTION!🔥 Shutting down...');
+  console.log('UNCAUGHT EXCEPTION! Shutting down...');
   console.log(err.name, err.message);
   process.exit(1);
 });
 
-app.set('port', process.env.PORT || 9000);
+const PORT = process.env.PORT || 9000;
 
-const server = app.listen(app.get('port'), async () => {
-  console.log(`App listening on port → ${server.address().port}`);
-});
+const server = app.listen(PORT, () => console.log(`APP LISTENING ON PORT ${PORT}`));
 
 process.on('unhandledRejection', err => {
-  console.log('UNHANDLED REJECTION!🔥 Shutting down...');
+  console.log('UNHANDLED REJECTION! Shutting down...');
   console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
