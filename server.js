78cbeb861fed9f7d82a79a8e4c70c522d12a0ad9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-06 15:31:08 +0100

    fix: refactor

--- server.js
+++ server.js
@@ -2,17 +2,19 @@ const app = require('./app');
 require('./startup/db')();
 
 process.on('uncaughtException', err => {
-  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+  console.log('UNCAUGHT EXCEPTION!🔥 Shutting down...');
   console.log(err.name, err.message);
   process.exit(1);
 });
 
-const PORT = process.env.PORT || 9000;
+app.set('port', process.env.PORT || 9000);
 
-const server = app.listen(PORT, () => console.log(`APP LISTENING ON PORT ${PORT}`));
+const server = app.listen(app.get('port'), () => {
+  console.log(`App listening on port →  ${server.address().port}`);
+});
 
 process.on('unhandledRejection', err => {
-  console.log('UNHANDLED REJECTION! Shutting down...');
+  console.log('UNHANDLED REJECTION!🔥 Shutting down...');
   console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
